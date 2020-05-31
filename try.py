f92ab00c95f123f06b6c7a2becb67bcb390a439f
Saksham Agarwal <61929425+Saksham-Agarwal231@users.noreply.github.com>
2020-05-31 11:06:38 +0530

    hello world

---
+++ try.py
@@ -0,0 +1,19 @@
+import random
+
+take = int(input("Enter the maximim no here: "))
+
+yguess = random.randint(1, take)
+
+for n in range(3):
+    mguess = int(input("Enter your guess here: "))
+
+    if mguess = yguess:
+        print("Kuddos!! your guess is right!!")
+
+
+    elif yguess > mguess:
+        print("Your guess is low!!")
+
+
+    elif yguess < mguess:
+        print("Your guess is high!!")
